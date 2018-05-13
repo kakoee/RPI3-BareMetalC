66dfec4e6a6c0ad5214f33798396bc42980672f7
Reza Kakoee <kakoee@gmail.com>
2018-05-13 11:27:37 -0700

    updated

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,5 @@
+## alias arm-gcc='arm-none-eabi-gcc --specs=nosys.specs -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8 -mneon-for-64bits'
+
 arm-gcc -nostdlib -nostartfiles -ffreestanding -c main.c 
 
 
